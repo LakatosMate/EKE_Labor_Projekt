1b6090b9836f91cece9db723d1aaf461dfcd20d9
Lakatos Máté <lmateka17@gmail.com>
2024-12-02 12:31:44 +0100

    Minta kezdőoldal megjenenítése routing és controlleren keresztül

---
+++ LabProjekt/resources/views/Kezdőlap.blade.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="hu">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Kezdőoldal</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEJf+U+f2Q4Zb8qk7V5F0JibK3o+kblbZx5iQmvnFVvPU5d6OB5PT0JIt7e4a" crossorigin="anonymous">
+</head>
+<body>
+    <!-- Navigációs menü -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">My Laravel App</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link active" href="#">Főoldal</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Szolgáltatások</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Kapcsolat</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <!-- Kezdőoldal tartalom -->
+    <div class="container mt-5">
+        <h1>Üdvözlünk a Laravel alkalmazásban!</h1>
+        <p>Ez egy alapértelmezett kezdőoldal Bootstrap navigációs menüvel.</p>
+    </div>
+
+    <!-- Bootstrap JS -->
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz4fnFO9gyb9bNx3gJz1v1lmhX4UwM9INhljS8l6Wx7Odl4PzbeVQj+2j4" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js" integrity="sha384-pzjw8f+ua7Kw1TIq0Fw9lPpXc46b4a7vdTp1kK1jZvIuopLw6K66T4u0t0p5kFFl" crossorigin="anonymous"></script>
+</body>
+</html>
