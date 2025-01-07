bc9995ab093ba9f870d6f88f3c19e3b9ebc4b3aa
Lakatos Máté <lmateka17@gmail.com>
2025-01-07 19:17:46 +0100

    Felhasználói bejelentkezés form 0.1

--- LabProjekt/resources/views/login.blade.php
+++ LabProjekt/resources/views/login.blade.php
@@ -5,6 +5,19 @@ Bejelentkezés
 @endsection
 
 @section('content')
-  <h1>Üdvözöllek a bejelentkezés oldalon!</h1>
-  <p>Bejelentkezés.</p>
+ <div class="container">
+    <h2>Bejelentkezés</h2>
+    <form method="POST" action="{{ route('login') }}">
+        @csrf
+        <div class="mb-3">
+            <label for="username" class="form-label">Felhasználó név</label>
+            <input type="text" class="form-control" id="username" name="username" required>
+        </div>
+        <div class="mb-3">
+            <label for="password" class="form-label">Jelszó</label>
+            <input type="password" class="form-control" id="password" name="password" required>
+        </div>
+        <button type="submit" class="btn btn-primary">Bejelentkezés</button>
+    </form>
+</div>
 @endsection
