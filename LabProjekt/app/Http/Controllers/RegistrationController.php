db3ecaf24b8ddcb74d6b30ecda3c1809c085dbb0
Ferenc Simon <fsimon@visionlab.hu>
2025-01-08 15:06:16 +0100

    Regisztrácós logika hozzáadása a kontrollerbe hibakezeléssel

--- LabProjekt/app/Http/Controllers/RegistrationController.php
+++ LabProjekt/app/Http/Controllers/RegistrationController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User; 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class RegistrationController extends Controller
 {
@@ -13,12 +15,29 @@ class RegistrationController extends Controller
     public function registration(Request $request)
     {
         try {
-            // Logika építése
-            echo 'Regisztráció indítása...';
 
-            return redirect('login')->with('success','Felhasználót sikeresen létrehoztuk most már bejelentkezhet.');;
+            $request->validate([
+                'username' => 'required|string|unique:users|max:200',
+                'email' => 'required|string',
+                'password' => 'required|confirmed|min:8|max:16',
+            ],
+            [
+                'username.unique' => 'Ez a felhasználónév már foglalt. Válasszon egy másikat!',
+                'password.min' => 'A jelszónak legalább 8 karakter hosszúnak kell lennie.',
+                'password.max' => 'A jelszó nem lehet hosszabb 16 karakternél.',
+                'password.confirmed' => 'A jelszavak nem egyeznek meg. Ellenőrizze!'
+            ]);
+
+            $new_user = new User;
+            $new_user->username = $request->username;
+            $new_user->email = $request->email;
+            $new_user->password = Hash::make($request->password);
+            $new_user->save();
+
+            return redirect('login')->with('success','Felhasználót sikeresen létrehoztuk most már bejelentkezhet.');
+
         } catch (\Exception $exception) {
-            return redirect('/')->with('fail',$exception->getMessage());
+            return redirect('register')->with('fail',$exception->getMessage());
         }
         
     }
--- LabProjekt/resources/views/register.blade.php
+++ LabProjekt/resources/views/register.blade.php
@@ -7,23 +7,40 @@ Regisztráció
 @section('content')
   <div class="container">
     <h2>Regisztráció</h2>
+    @if (Session::has('fail'))
+      <div class="my-3">
+        <span class="alert alert-danger p-2">{{Session::get('fail')}}</span>
+      </div>
+    @endif
     <form method="POST" action="{{ route('registration') }}">
       @csrf
       <div class="mb-3">
-          <label for="username" class="form-label">Felhasználó név</label>
-          <input type="text" class="form-control" id="username" name="username" required>
+          <label for="username" class="form-label">Felhasználónév</label>
+          <input type="text" placeholder="Írja be a felhasználónevet" class="form-control" id="username" name="username" required>
+          @error('username')
+            <span class="text-danger">{{$message}}</span>
+          @enderror
       </div>
       <div class="mb-3">
         <label for="email" class="form-label">Email cím</label>
-        <input type="email" class="form-control" id="email" name="email" required>
+        <input type="email" placeholder="Írja be az Email címét" class="form-control" id="email" name="email" required>
+        @error('email')
+          <span class="text-danger">{{$message}}</span>
+        @enderror
       </div>
       <div class="mb-3">
           <label for="password" class="form-label">Jelszó</label>
-          <input type="password" class="form-control" id="password" name="password" required>
+          <input type="password" placeholder="Írja be a jelszót" class="form-control" id="password" name="password" required>
+          @error('password')
+            <span class="text-danger">{{$message}}</span>
+          @enderror
       </div>
       <div class="mb-3">
         <label for="password_confirmation" class="form-label">Jelszó megerősítés</label>
-        <input type="password" class="form-control" id="password_confirmation" name="password_confirmation" required>
+        <input type="password" placeholder="Írja be újra a jelszót" class="form-control" id="password_confirmation" name="password_confirmation" required>
+        @error('password_confirmation')
+          <span class="text-danger">{{$message}}</span>
+        @enderror
       </div>
       <button type="submit" class="btn btn-primary">Regisztráció</button>
   </form>
