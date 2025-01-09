7287f297467c31ca8c4ca2e914ffcdd713e0a62a
Ferenc Simon <fsimon@visionlab.hu>
2025-01-09 10:41:09 +0100

    Profil megjelenítési javítások

--- LabProjekt/resources/views/dashboard.blade.php
+++ LabProjekt/resources/views/dashboard.blade.php
@@ -9,7 +9,11 @@
     <h2>Írányítópult</h2>
 
     <div class="mt-3">
-        <h4>Üdvözöllek kedves, {{ Auth::user()->full_name }}!</h4>
+        @if (empty(Auth::user()->full_name))
+        <h4>Üdvözöllek!</h4>
+        @else
+            <h4>Üdvözöllek kedves, {{ Auth::user()->full_name }}!</h4>
+        @endif
         <p>Legyen szép napod!</p>
     </div>
 </div>
--- LabProjekt/resources/views/profile/profile.blade.php
+++ LabProjekt/resources/views/profile/profile.blade.php
@@ -7,5 +7,11 @@ Profilom
 @section('content')
  <div class="container">
     <h2>Profilom</h2>
+    <div class="mt-3">
+        @if (empty(Auth::user()->full_name) == false)
+            <h5>{{ Auth::user()->full_name }}</h5>
+        @endif 
+        <p>Felhasználóvév: {{ Auth::user()->username }}</br>Email cím: {{ Auth::user()->email }}</br>Jogkör: {{ Auth::user()->role }}</p>
+    </div>
 </div>
 @endsection
