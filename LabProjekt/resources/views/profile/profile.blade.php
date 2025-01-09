b3ea498c789dd12afd080dfd92bbc00966d1074e
Ferenc Simon <fsimon@visionlab.hu>
2025-01-09 11:47:13 +0100

    Profil oldal modósítása

--- LabProjekt/resources/views/profile/profile.blade.php
+++ LabProjekt/resources/views/profile/profile.blade.php
@@ -5,13 +5,33 @@ Profilom
 @endsection
 
 @section('content')
- <div class="container">
-    <h2>Profilom</h2>
-    <div class="mt-3">
-        @if (empty(Auth::user()->full_name) == false)
-            <h5>{{ Auth::user()->full_name }}</h5>
-        @endif 
-        <p>Felhasználóvév: {{ Auth::user()->username }}</br>Email cím: {{ Auth::user()->email }}</br>Jogkör: {{ Auth::user()->role }}</p>
+
+<div class="container">
+    <div class="container rounded bg-white mt-3 mb-3">
+        <div class="row">
+            <div class="col-md-3 border-right">
+                <div class="d-flex flex-column align-items-center text-center p-3 py-5">
+                    <img class="rounded-circle" width="150px" src="{{ asset('storage/images/dummy-profile-pic-300x300.png') }}" alt="Profilkép">
+                    <span class="font-weight-bold">{{ Auth::user()->username }}</span>
+                    <span class="text-black-50">{{ Auth::user()->email }}</span>
+                    <span class="font-weight-bold">Jogkör: {{ Auth::user()->role }}</span>
+                    <span> </span>
+                </div>
+            </div>
+            <div class="col-md-5 border-right">
+                <div class="p-3 py-5">
+                    <div class="d-flex justify-content-between align-items-center mb-3">
+                        <h2 class="text-right">Profilom</h2>
+                    </div>
+                    <div class="row mt-3">
+                        @if (empty(Auth::user()->full_name) == false)
+                            <h5>{{ Auth::user()->full_name }}</h5>
+                        @endif 
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
+
 @endsection
