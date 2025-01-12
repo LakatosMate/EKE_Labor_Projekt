239b7df614b6b6617f851db72fa5345cd21de7a3
Ferenc Simon <fsimon@visionlab.hu>
2025-01-12 19:19:37 +0100

    web root javítása

--- LabProjekt/routes/web.php
+++ LabProjekt/routes/web.php
@@ -4,11 +4,10 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\RegistrationController;
-<<<<<<< HEAD
+
 use App\Http\Controllers\PostController;
-=======
 use App\Http\Controllers\ProfileController;
->>>>>>> fc783c611b25576dbd7bffa970f2e9329d38c6d3
+
 
 Route::get('/', [HomeController::class, 'index']);
 Route::get('login',[AuthController::class,'login'])->name('login');
@@ -16,10 +15,10 @@ Route::post('logging_in',[AuthController::class,'logging_in'])->name('logging_in
 Route::get('register',[RegistrationController::class,'register'])->name('register');
 Route::post('registration',[RegistrationController::class,'registration'])->name('registration');
 
-<<<<<<< HEAD
+
 Route::resource('post', PostController::class);
 //Middlaware behozatala.
-=======
+
 Route::get('dashboard', function () {
     return view('dashboard');
 })->name('dashboard')->middleware('auth');
@@ -29,4 +28,4 @@ Route::get('profile',[ProfileController::class,'profile'])->name('profile')->mid
 Route::post('logout', [AuthController::class, 'logout'])->name('logout');
 
 Route::post('profile-picture', [ProfileController::class, 'updateProfilePicture'])->name('profile.picture.update');
->>>>>>> fc783c611b25576dbd7bffa970f2e9329d38c6d3
+
