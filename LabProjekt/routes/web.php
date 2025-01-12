3108602fde4114f185fdc9a24452b8a5da56ea34
Földvári Ádám <f.adam.1995@gmail.com>
2025-01-12 19:13:08 +0100

    web route módosítás

--- LabProjekt/routes/web.php
+++ LabProjekt/routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\RegistrationController;
+use App\Http\Controllers\PostController;
 
 Route::get('/', [HomeController::class, 'index']);
 Route::get('login',[AuthController::class,'login'])->name('login');
@@ -11,4 +12,5 @@ Route::post('logging_in',[AuthController::class,'logging_in'])->name('logging_in
 Route::get('register',[RegistrationController::class,'register'])->name('register');
 Route::post('registration',[RegistrationController::class,'registration'])->name('registration');
 
+Route::resource('post', PostController::class);
 //Middlaware behozatala.
